bf5a20dd47cceb18c7a16899a7621b2711519fe6
antasth <antasth@mail.ru>
2024-01-27 15:31:06 +0300

    feat: implement streams:read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
-const read = async () => {
-    // Write your code here 
-};
+import { createReadStream } from 'node:fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-await read();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const filePath = path.join(__dirname, 'files/fileToRead.txt')
+
+const read = async () => {
+  const fileReadStream = createReadStream(filePath)
+  fileReadStream.on('data', (chunk) => process.stdout.write(chunk + '\n'))
+}
+
+await read()

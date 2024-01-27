93250eaadd5998c77d132f810e345a8a2a386549
antasth <antasth@mail.ru>
2024-01-27 15:14:45 +0300

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createReadStream } from 'node:fs'
+import crypto from 'crypto'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-await calculateHash();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const filePath = path.join(__dirname, 'files/fileToCalculateHashFor.txt')
+
+const calculateHash = async () => {
+  const fileReadStream = createReadStream(filePath)
+  const hash = crypto.createHash('sha256')
+
+  fileReadStream.on('data', (data) => {
+    hash.update(data)
+  })
+
+  fileReadStream.on('end', () => {
+    console.log(hash.digest('hex'))
+  })
+}
+
+await calculateHash()

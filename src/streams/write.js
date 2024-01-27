cbd268d6cb3eb62a1ce2d29a52c3c6561a5c92e0
antasth <antasth@mail.ru>
2024-01-27 17:48:54 +0300

    feat: implement streams:write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
-const write = async () => {
-    // Write your code here 
-};
+import { createWriteStream } from 'node:fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const filePath = path.join(__dirname, 'files/fileToWrite.txt')
 
-await write();
+const write = async () => {
+  console.log('Hello, enter your data(for exit press CTRL + C):')
+  const fileWriteStream = createWriteStream(filePath)
+  process.stdin.on('data', (chunk) => {
+    fileWriteStream.write(chunk)
+  })
+}
+await write()
